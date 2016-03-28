2aff4c19317cc3d2aba619aa1b4027e314180075
Rai Singh <rsingh@honeybadger.local>
2016-03-28 08:32:41 -0400

    Attempting to use JSON file as a datasource and filter content based upon a request parameter.

--- app/views.py
+++ app/views.py
@@ -8,24 +8,33 @@ Created on Mar 19, 2016
 
 from app import app
 from flask.templating import render_template
-import json
+from flask import request
+import json 
 
 title = 'TaskTrack'
+user = {'nickname': 'John Smith'} #fake user
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'John Smith'} #fake user
-    with open('app/templates/posts.json') as posts_file:
-        posts = json.load(posts_file)
-    
-    return render_template('index.tmpl',
+    with open('app/templates/tasks.json') as tasks_file:
+        tasks = json.load(tasks_file)
+        
+    return render_template('index.html',
                            user=user,
-                           posts=posts)
+                           tasks=tasks)
 
-@app.route('/editpost')
+@app.route('/edittask')
 def editpost():
-    return render_template('editpost.tmpl')    
+    taskid = request.args.get('id')
+    with open('app/templates/tasks.json') as tasks_file:
+        # Filter python objects with list comprehensions
+#         filtered_task = [x for x in tasks_file if x['taskid'] == taskid]
+        filtered_task = [x for x in tasks_file if get_by_taskid(x,taskid)]
+        task = json.dumps(filtered_task)
+    return render_template('edittask.html', 
+                           id=id, 
+                           task=task)    
     
 @app.context_processor    
 def utility_processor():
@@ -34,4 +43,12 @@ def utility_processor():
     def get_title():
         return title if title != '' else 'No title here' 
     return dict(format_version=format_version,
-                get_title=get_title)
+                get_title=get_title,
+                user=user)
+    
+    
+def get_by_taskid(tasks, taskid):
+    try:
+        return tasks['taskid'] == taskid
+    except (KeyError, TypeError):
+        return False    
